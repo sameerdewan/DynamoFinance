7d1a4a3bdd5f6cf3e4ad7a08824da9d1831b3dc8
sameerdewan <sameer.dewan@itential.com>
2021-02-24 22:14:49 -0500

    base contract

--- contracts/DynamoFinance.sol
+++ contracts/DynamoFinance.sol
@@ -6,11 +6,34 @@ contract DynamoFinance {
     address public appAddress;
     bool public operational = false;
     mapping(address => bool) public contracts;
+    mapping (address => bool) public allowed;
     constructor() public {
         owner = msg.sender;
         appAddress = address(this);
     }
-    function isValidContract(address _address) external view returns(bool) {
-        return contracts[_address] == true;
+    modifier onlyAllowed() {
+        require(allowed[msg.sender] == true || msg.sender == owner, 'ERROR: AUTH');
+        _;
+    }
+    function addAllowed(address _address) external onlyAllowed() {
+        allowed[_address] = true;
+    }
+    function removeAllowed(address _address) external onlyAllowed() {
+        allowed[_address] = false;
+    }
+    function enable() external onlyAllowed() {
+        operational = true;
+    }
+    function disable() external onlyAllowed() {
+        operational = false;
+    }
+    function addContract(address _address) external onlyAllowed() {
+        contracts[_address] = true;
+    }
+    function removeContract(address _address) external onlyAllowed() {
+        contracts[_address] = false;
+    }
+    function safeCallFn() external view returns(bool) {
+        return operational == true && contracts[msg.sender] == true;
     }
 }
